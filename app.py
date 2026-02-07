ba142265d608551def00f95b9f5bd98b214f64d0
GilbranAlfarizi <gilbranalfarizi02@gmail.com>
2026-02-07 13:56:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -61,9 +61,9 @@ div[data-testid="stMetricValue"] {
 div[data-testid="stDataFrame"] {
     background-color: #020617;
     border-radius: 16px;
-    border: 1px solid #1e293b;
+    border: 10px solid #1e293b;
     padding: 14px;
-    box-shadow: 0 100px 30px rgba(0,0,0,0.45);
+    box-shadow: 0 12px 30px rgba(0,0,0,0.45);
 }
 
 /* grid */
@@ -278,6 +278,7 @@ with col_comp:
         }
 
         st_echarts(option, height="300px")
+
 
 
 
